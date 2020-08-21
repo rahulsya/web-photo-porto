8ade3060bfae071b3eea4973d4d1bfcc991a0d29
rahulsya <48597821+rahulsya@users.noreply.github.com>
2020-08-21 23:45:50 +0700

    fix alt image

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,7 @@ export default function About() {
             <img
               classname="rounded-lg shadow-lg antialiased"
               src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-              alt="profile image"
+              alt="profile"
             />
           </div>
           <div className="px-4 py-2 font-semibold flex flex-col text-gray-700">
