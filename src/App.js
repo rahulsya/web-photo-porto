13821d85437e3638b237279915a500e5a7158747
rahulsya <48597821+rahulsya@users.noreply.github.com>
2020-08-29 23:42:47 +0700

    fix 404 page

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import DetailPhoto from "./pages/detailPhoto";
-import error from "./pages/ErrorPage";
+import Error from "./pages/ErrorPage";
 
 function App() {
   return (
     <>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/photo/:id" component={DetailPhoto} />
+        <Route exact path="/photo/:id" component={DetailPhoto} />
         <Route exact path="/about" component={About} />
-        <Route component={error} />
+        <Route component={Error} />
       </Switch>
     </>
   );
